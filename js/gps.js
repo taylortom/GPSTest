c49755e46c5021e37665de093c858d956d004a80
Thomas Taylor <dev@taylortom.co.uk>
2014-02-25 12:54:00 +0000

    Set up basic tracking

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,39 @@
-$(document).ready(function() 
-{
+$(document).ready(function() {
+
 	$("#output").html('Hello world.');
+	
+	// vars
+	var watchID;
+	var options = {};
+	var positions = [];
+	var maxPositions = 10;
+	
+	if ("geolocation" in navigator) _initGPS();
+	else console.log('geolocation not supported....');
+	
+	function _initGPS(){
+		
+		console.log('_initGPS');
+		watchID = navigator.geolocation.watchPosition(onLocationSuccess, onLocationError, options);
+	}
+	
+	/**
+	 * Note: these only fire when the location changes...
+	 */
+	
+	function onLocationSuccess(position){
+		
+		console.log('onLocationSuccess: ' + position.coords + " [" + position.timestamp + "]");
+		
+		if(positions.length < maxPositions) 
+		{
+			positions.push(position);
+		}
+		else navigator.geolocation.clearWatch(watchID);
+	}
+	
+	function onLocationError(error){
+		
+		console.log('onLocationError: ' + error.code + ": " + error.message);
+	}
 });
