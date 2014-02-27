5c1a30852043b8bc326324f0894fdb5de106d008
Thomas Taylor <tom@taylortom.co.uk>
2014-02-27 22:09:32 +0000

    Fixed tracking

--- js/gps.js
+++ js/gps.js
@@ -3,42 +3,72 @@ $(document).ready(function() {
 	console.log('document.ready');
 
 	// vars
+	var track_count = "track_count";
 	var $output = $("#output");
-	var watchID;
 	var positions = [];
+	var watchID;
 	var options = { 
 		enableHighAccuracy: true, 
-		maximumAge        : 10000, 
 		timeout           : 10000
 	};
 	
-	if ("geolocation" in navigator) _initGPS();
+	if ("geolocation" in navigator) {
+		_initButtons();
+	}
 	else console.log('geolocation not supported....');
 	
-	function _initGPS(){
-		
-		console.log('_initGPS');
-		watchID = navigator.geolocation.watchPosition(onLocationSuccess, onLocationError, options);
+	function _initButtons(){
+
+		$('.start').click(_startTracking);
+		$('.stop').click(_stopTracking);
+		$('.map').click(_drawMap);
 	}
-	
-	/**
-	 * Note: these only fire when the location changes...
-	 */
-	
-	function onLocationSuccess(position){
+
+	function _startTracking(){
 		
-		var time = new Date(position.timestamp);
-		console.log("onLocationSuccess: at " + time.toTimeString());
-		
-		$output.append("<li>long: " + position.coords.longitude + "lat: " + position.coords.latitude + " at " + time.toTimeString() + "</li>")
-		positions.push(position);
-		
-		// need to call this to end it all...........
-		// navigator.geolocation.clearWatch(watchID);
+		console.log('_startTracking');
+
+		_reset();
+
+		watchID = navigator.geolocation.watchPosition(function(position) {
+
+			var time = new Date(position.timestamp);
+
+			$output.append("<li>long: " + position.coords.longitude + "lat: " + position.coords.latitude + " at " + time.toTimeString() + "</li>")
+			positions.push(position);
+			
+		}, 
+			function(){ console.log('onLocationError: ' + error.code + ": " + error.message);
+	    }, options);
 	}
-	
-	function onLocationError(error){
-		
-		console.log('onLocationError: ' + error.code + ": " + error.message);
+
+	function _stopTracking(){
+
+		console.log('_stopTracking');
+
+		navigator.geolocation.clearWatch(watchID);
+		watchID = null;
+
+		var trackCount = parseInt(window.localStorage.getItem(track_count));
+		var trackID = (!Number.isNaN(trackCount)) ? trackCount+1 : 1;
+
+		window.localStorage.setItem(trackID, JSON.stringify(positions));
+		window.localStorage.setItem(track_count, trackID);
+
+		_reset();
+	}
+
+	function _drawMap(){
+
+		console.log('_drawMap');
+
+		var lastID = parseInt(window.localStorage.getItem(track_count));
+		console.log(lastID + ": " + window.localStorage.getItem(lastID));
+	}
+
+	function _reset(){
+
+		positions = [];
+		$output.html('');
 	}
 });
